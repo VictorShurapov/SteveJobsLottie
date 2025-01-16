b7bca119076b21502e2e7ee1906010efd17033ff
Viktor <vshurapov@gmail.com>
2025-01-16 17:42:20 +0200

    Initial Commit

---
+++ SteveJobsLottie/SteveJobsLottieApp.swift
@@ -0,0 +1,17 @@
+//
+//  SteveJobsLottieApp.swift
+//  SteveJobsLottie
+//
+//  Created by Yaro4ka on 16.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct SteveJobsLottieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
