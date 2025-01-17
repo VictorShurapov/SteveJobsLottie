13bf89c4a3b2f9cdf6f8330b29b0167c8677f4e6
Viktor <vshurapov@gmail.com>
2025-01-17 15:16:30 +0200

    feat: Add Steve Jobs Lottie animation with dynamic color scheme support
    - Installed Lottie using Swift Package Manager for smaller repo size and faster downloads.
      - Used the `lottie-spm` repo for Swift Package Manager support to avoid downloading the full `lottie-ios` repository.
      - `lottie-spm` repo references a precompiled XCFramework (~8MB) included in the latest `lottie-ios` release.
    
    - Added dynamic Lottie animation support based on color scheme:
      - Created `steveJobsAnimationName` in a `ColorScheme` extension to dynamically select animations ("SteveJobsBright" for light mode, "SteveJobsDark" for dark mode).
      - Used a button to toggle between light and dark modes, updating the UI and animation accordingly.
    
    - Updated UI:
      - Added a title, an icon-based toggle button, and a text footer.
      - Integrated LottieView with looping animation.

--- SteveJobsLottie/ContentView.swift
+++ SteveJobsLottie/ContentView.swift
@@ -6,16 +6,36 @@
 //
 
 import SwiftUI
+import Lottie
 
 struct ContentView: View {
+    @Environment(\.colorScheme) var colorScheme
+    @State private var useDarkMode = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Steve Jobs Lottie")
+                .font(.title)
+            Button(action: {
+                useDarkMode.toggle()
+            }) {
+                Image(systemName: useDarkMode ? "sun.max.fill" : "moon.fill")
+                    .font(.title)
+                    .foregroundColor(useDarkMode ? .yellow : .blue)
+            }
+            .padding(5)
+            .background(Circle().fill(useDarkMode ? Color.gray : Color.white).shadow(radius: 5))
+            
+            LottieView(animation: .named(colorScheme.steveJobsAnimationName))
+                .playing(loopMode: .loop)
+            Text("Attention is the ultimate asset.")
+                .font(.title)
+                .lineLimit(1)
+                .minimumScaleFactor(0.5)
+                .padding(.bottom, 5)
         }
         .padding()
+        .preferredColorScheme(useDarkMode ? .dark : .light)
     }
 }
 
---
+++ SteveJobsLottie/Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  Extensions.swift
+//  SteveJobsLottie
+//
+//  Created by Yaro4ka on 17.01.2025.
+//
+
+import SwiftUI
+
+extension ColorScheme {
+    var steveJobsAnimationName: String {
+        return self == .dark ? "SteveJobsBright" : "SteveJobsDark"
+    }
+}
